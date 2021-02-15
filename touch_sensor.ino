f3bca2a54189b77b8a139bf38a3361ed22fcbef0
Zachrandika Alif Syahreza <alifsyr@gmail.com>
2021-02-15 17:53:00 +0700

    Add files via upload

---
+++ touch_sensor.ino
@@ -0,0 +1,38 @@
+// set nomor pin
+const int touchPin = 4; 
+const int ledPin1 = 16;
+const int ledPin2 = 17;
+
+const int threshold = 20;
+
+// variabel untuk menyimpan nilai touch pin
+int touchValue;
+
+void setup(){
+  Serial.begin(115200);
+  delay(1000);
+  // menginisiasi ledpin1 dan ledpin2 sebagai output
+  pinMode (ledPin1, OUTPUT);
+  pinMode (ledPin2, OUTPUT);
+}
+
+void loop(){
+  // membaca state yang dihasilkan dari nilai touchRead
+  touchValue = touchRead(touchPin);
+  Serial.print(touchValue);
+  // melakukan pengecekan apakah nilai treshold lebih kecil dari nilai touchValue
+  // jika iya maka menyalakan LED
+  if(touchValue < threshold){
+    // menyalakan LED
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, HIGH);
+    Serial.println(" - LED on");
+  }
+  else{
+    // mematikan LED
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, LOW);
+    Serial.println(" - LED off");
+  }
+  delay(500);
+}
